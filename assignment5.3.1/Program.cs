29baee9651096df8f35323f5bc1c83a1dc8f3279
connor gammill <connor.gammill@outlook.com>
2024-12-04 12:31:55 -0500

    Add project files.

---
+++ assignment5.3.1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace assignment5._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] flowerbed = { 1, 0, 0, 0, 1};
+			Console.WriteLine("Enter how many flowers you want to plant in the pot");
+            int n = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine(CanPlaceFlowers(flowerbed, n)); 
+        }
+     
+
+		static bool CanPlaceFlowers(int[] flowerbed, int n)
+		{
+			var count = 0;
+			for (int i = 0; i < flowerbed.Length; i++)
+			{
+				if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+				{
+					flowerbed[i++] = 1;
+					count++;
+				}
+				if (count >= n)
+				{
+					return true;
+				}
+			}
+			return count >= n;
+		}
+    }
+}
